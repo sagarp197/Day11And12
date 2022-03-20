1bed58d8f12696421467884ec57749d3a5906e4d
sagarp197 <sagarp197@gmail.com.com>
2022-03-20 15:39:16 +0530

    Rice

---
+++ Rice.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONInventoryManagement
+{
+    public class Rice
+    {
+        public List<TypesOfRice> typesOfRice;
+    }
+    
+    public class TypesOfRice
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Price { get; set; }
+
+    }
+
+    public class Pulses
+    {
+        public List<TypesOfPulses> typesOfPulses;
+    }
+
+    public class TypesOfPulses
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Price { get; set; }
+
+    }
+
+    public class Wheats
+    {
+        public List<TypesOfWheats> typesOfWheats;
+    }
+    public class TypesOfWheats
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Price { get; set; }
+
+    }
+}
