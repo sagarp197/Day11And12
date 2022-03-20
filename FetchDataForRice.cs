1b9e7e82373d6da537763eba75c1960cd4c379c3
sagarp197 <sagarp197@gmail.com.com>
2022-03-20 15:40:55 +0530

    FetchDataForRice

---
+++ FetchDataForRice.cs
@@ -0,0 +1,71 @@
+﻿//using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONInventoryManagement
+{
+    public class FetchDataForPulses
+    {
+        public Pulses Read(string Path)
+        {
+            using (StreamReader file = new StreamReader(Path))
+            {
+
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Pulses>(json);
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+
+    public class FetchDataForRice
+    {
+        public Rice Read(string Path)
+        {
+            using (StreamReader file = new StreamReader(Path))
+            {
+
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Rice>(json);
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+
+    public class FetchDataForWheats
+    {
+        public Wheats Read(string Path)
+        {
+            using (StreamReader file = new StreamReader(Path))
+            {
+
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Wheats>(json);
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+
+}
